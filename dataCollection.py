5c4622cdada8b020da7875c4d01127fc6e2f8e02
thayna <arraisthayna2106@gmail.com>
2023-10-23 16:25:49 -0300

    overlay and save images

--- dataCollection.py
+++ dataCollection.py
@@ -1,16 +1,55 @@
 import cv2
-import mediapipe as mp
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+import time
 
-video = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
 
-hand = mp.solutions.hands
-Hand = hand.Hands(max_num_hands=1)
-mpDraw = mp.solutions.drawing_utils
+offset = 20
+imgSize = 300
+
+folder = "Data/A"
+counter = 0
 
 while True:
-    success, img = video.read()
-    imgRGB = cv2.cv2Color(img, cv2.COLOR_BGR2RGB)
-    results = Hand.process(imgRGB)
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+        imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
+
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h/w
+
+        if aspectRatio > 1:
+            k = imgSize/h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal+hGap, :] = imgResize
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Hands On Game!", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        print(counter)
